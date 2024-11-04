cf329c4398d22a676a44c9a19483bfd342e8bbf4
FrancisIje <ijenebefrancis@gmail.com>
2024-11-04 09:50:26 +0100

    style(text style): created the app text styles

---
+++ lib/views/utils/typography.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class AppTypography {
+  static const h4 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 32,
+    height: 35,
+    letterSpacing: -0.2,
+  );
+  static const h5 = TextStyle(
+    fontWeight: FontWeight.w700,
+    fontSize: 24,
+    height: 30,
+    letterSpacing: -0.2,
+  );
+  static const h6 = TextStyle(
+    fontWeight: FontWeight.w700,
+    fontSize: 20,
+    height: 25,
+    letterSpacing: -0.2,
+  );
+  static const bodyLargeSB = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 17,
+    height: 25,
+    letterSpacing: 0,
+  );
+  static const bodyLargeM = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 17,
+    height: 25,
+    letterSpacing: 0,
+  );
+  static const bodyLargeR = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 17,
+    height: 25,
+    letterSpacing: 0,
+  );
+  static const bodyMediumSB = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 15,
+    height: 20,
+    letterSpacing: 0,
+  );
+  static const bodyMediumM = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 15,
+    height: 20,
+    letterSpacing: 0,
+  );
+  static const bodyMediumR = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 15,
+    height: 20,
+    letterSpacing: 0,
+  );
+  static const bodySmallSB = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 13,
+    height: 8,
+    letterSpacing: 0,
+  );
+  static const bodySmallM = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 13,
+    height: 8,
+    letterSpacing: 0,
+  );
+  static const bodySmallR = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 13,
+    height: 8,
+    letterSpacing: 0,
+  );
+  static const captionR = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 11,
+    height: 18,
+    letterSpacing: 0,
+  );
+  static const captionM = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 11,
+    height: 18,
+    letterSpacing: 0,
+  );
+}

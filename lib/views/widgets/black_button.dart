0a10554d8e03c723fc104ff1e328e4589e15487d
FrancisIje <ijenebefrancis@gmail.com>
2024-11-04 20:44:51 +0100

    feat(onboarding): developed the onboarding screens

---
+++ lib/views/widgets/black_button.dart
@@ -0,0 +1,29 @@
+import 'package:filllo_mobile/views/utils/colours.dart';
+import 'package:filllo_mobile/views/utils/mediaquery.dart';
+import 'package:filllo_mobile/views/utils/typography.dart';
+import 'package:flutter/material.dart';
+
+class BlackButton extends StatelessWidget {
+  final String text;
+  final Function() onTap;
+  const BlackButton({super.key, required this.text, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.symmetric(
+            vertical: SizeConfig.scaleHeight(18),
+            horizontal: SizeConfig.scaleHeight(82)),
+        decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(80)),
+            color: AppColours.dark),
+        child: Text(
+          text,
+          style: AppTypography.bodyLargeSB.copyWith(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}

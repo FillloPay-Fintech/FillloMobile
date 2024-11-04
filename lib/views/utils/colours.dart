4a9a1a3078c418bd9925a3605068c0c011ca09a0
FrancisIje <ijenebefrancis@gmail.com>
2024-11-04 10:34:20 +0100

    style(colours): created genral app colours

---
+++ lib/views/utils/colours.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColours {
+  static const brightOrange = Color(0xFFFF6C2E);
+  static const blueFlower = Color(0xFF382FB8);
+  static const dark = Color(0xFF121212);
+  static const galacticFederation = Color(0xFF360676);
+}

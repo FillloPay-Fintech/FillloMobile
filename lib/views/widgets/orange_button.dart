4dd2a1376305ec0137a14de0adc36cf8f7c8d43b
FrancisIje <ijenebefrancis@gmail.com>
2024-11-05 21:13:02 +0100

    feat(button): Orange button created

---
+++ lib/views/widgets/orange_button.dart
@@ -0,0 +1,31 @@
+import 'package:filllo_mobile/views/utils/colours.dart';
+import 'package:filllo_mobile/views/utils/mediaquery.dart';
+import 'package:filllo_mobile/views/utils/typography.dart';
+import 'package:flutter/material.dart';
+
+class OrangeButton extends StatelessWidget {
+  final String text;
+  final Function() onTap;
+  const OrangeButton({super.key, required this.text, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: SizeConfig.scaleHeight(61),
+        width: SizeConfig.scaleWidth(200),
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(80)),
+          color: AppColours.brightOrange,
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: AppTypography.bodyLargeSB.copyWith(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}

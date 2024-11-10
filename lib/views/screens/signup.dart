7d6d2ab6354e44b6e65cd73c5628d84d24f0e7e5
FrancisIje <ijenebefrancis@gmail.com>
2024-11-10 02:31:14 +0100

    fix(signup): refactor

--- lib/views/screens/signup.dart
+++ lib/views/screens/signup.dart
@@ -3,9 +3,9 @@ import 'package:filllo_mobile/views/screens/create_account.dart';
 import 'package:filllo_mobile/views/utils/colours.dart';
 import 'package:filllo_mobile/views/utils/mediaquery.dart';
 import 'package:filllo_mobile/views/utils/typography.dart';
+import 'package:filllo_mobile/views/widgets/error_toast.dart';
 import 'package:filllo_mobile/views/widgets/orange_button.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:gap/gap.dart';
 import 'package:provider/provider.dart';
 
@@ -179,16 +179,7 @@ class SignupScreen extends StatelessWidget {
                             text: "Continue",
                             onTap: () {
                               if (value.accountType == null) {
-                                return Fluttertoast.showToast(
-                                    msg: "Select Account Type",
-                                    fontAsset:
-                                        "assets/fonts/sf_pro_display/SFPRODISPLAYBOLD.otf",
-                                    toastLength: Toast.LENGTH_LONG,
-                                    gravity: ToastGravity.TOP,
-                                    timeInSecForIosWeb: 1,
-                                    backgroundColor: Colors.red,
-                                    textColor: Colors.white,
-                                    fontSize: SizeConfig.scaleText(16));
+                                return errorToast("Select Account Type");
                               } else {
                                 Navigator.of(context).push(MaterialPageRoute(
                                   builder: (context) =>
---
+++ lib/views/widgets/error_toast.dart
@@ -0,0 +1,15 @@
+import 'package:filllo_mobile/views/utils/mediaquery.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+errorToast(String msg) {
+  return Fluttertoast.showToast(
+      msg: "Select Account Type",
+      fontAsset: "assets/fonts/sf_pro_display/SFPRODISPLAYBOLD.otf",
+      toastLength: Toast.LENGTH_LONG,
+      gravity: ToastGravity.TOP,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.red,
+      textColor: Colors.white,
+      fontSize: SizeConfig.scaleText(16));
+}
